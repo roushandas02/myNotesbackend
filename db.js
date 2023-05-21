1242ea40243388fb3973ae58150213832d5086c4
roushandas02 <roushandas02@gmail.com>
2023-05-21 15:09:31 +0530

    changes2

--- db.js
+++ db.js
@@ -2,7 +2,8 @@
 
 const DATABASE = process.env.DATABASE
 const mongoose=require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/myNotes?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0";
+// const mongoURI = "mongodb://127.0.0.1:27017/myNotes?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0";
+const mongoURI = "mongodb+srv://roushandas02:roushan1@cluster0.ikc5ira.mongodb.net/myNotes";
 
 const connectToMongo = ()=>{
     mongoose.set('strictQuery', true);
